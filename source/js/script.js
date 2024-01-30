356bd1fda4692646521f83e6075164d6fa793af7
swimer-girl <swimermail@gmail.com>
2024-01-30 22:52:10 +1000

    task 5

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.navigation');
+let navToggle = document.querySelector('.navigation__toggle');
+
+navMain.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('navigation--closed')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+  } else {
+    navMain.classList.add('navigation--closed');
+    navMain.classList.remove('navigation--opened');
+  }
+});
